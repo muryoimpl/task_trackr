c1c9ba40a54857c3dcd69bdc2e244e90e73df798
muryoimpl <muryoimpl@gmail.com>
2015-10-09 01:14:44 +0900

    Add redux-devtools

--- main.jsx
+++ main.jsx
@@ -2,16 +2,34 @@ import 'babel/polyfill';
 
 import React from 'react';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { compose, createStore } from 'redux';
 
 import App from './containers/App';
 import indexReducer from './reducers/index';
 
-let store = createStore(indexReducer);
+// for redux devtools ---
+// React components for Redux DevTools
+import { devTools, persistState } from 'redux-devtools';
+import { DevTools, DebugPanel, LogMonitor } from 'redux-devtools/lib/react';
+
+const finalCreateStore = compose(
+  // Provides support for DevTools:
+  devTools(),
+  // Lets you write ?debug_session=<name> in address bar to persist debug sessions
+  persistState(window.location.href.match(/[?&]debug_session=([^&]+)\b/))
+)(createStore);
+
+let store = finalCreateStore(indexReducer);
+// -----
 
 React.render(
-  <Provider store={store}>
-    {() => <App />}
-  </Provider>,
+  <div>
+    <Provider store={store}>
+      {() => <App />}
+    </Provider>
+    <DebugPanel top right bottom>
+      <DevTools store={store} monitor={LogMonitor} />
+    </DebugPanel>
+  </div>,
   document.getElementById('content')
 );
