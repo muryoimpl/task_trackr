19feee4282cee79ddea686356741fdbe5195ccbb
muryoimpl <muryoimpl@gmail.com>
2015-10-09 00:38:03 +0900

    Replace material-ui component to bootstrap component

--- containers/App.jsx
+++ containers/App.jsx
@@ -3,46 +3,29 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import _ from 'lodash';
 
-//material-ui
-//import * as mui from 'material-ui';
-import * as mui, { ToolBar, ToolbarGroup, DropDownMenu, FontIcon } from 'material-ui';
+import injectTapEventPlugin from 'react-tap-event-plugin';
 
-const ThemeManager = new mui.Styles.ThemeManager();
-const Colors = mui.Styles.Colors;
+//bootstrap
+import { Navbar, NavBrand, Tabs, Tab } from 'react-bootstrap';
 
 export default class App extends React.Component {
-  getChildContext() {
-    return {
-      muiTheme: ThemeManager.getCurrentTheme()
-    };
-  }
-
   render() {
-    const menuItems = [
-      { payload: '1', text: 'Sample' }
-    ];
-
     return (
       <div>
-        <ToolBar>
-          <ToolbarGroup key={0} float="left">
-            <FontIcon className="muidocs-icon-action-home" />
-            <DropDownMenu menuItems={menuItems} />
-            <FontIcon className="muidocs-icon-box-add" />
-          </ToolbarGroup>
-
-          <ToolbarGroup key={1} float="right">
-
-          </ToolbarGroup>
-        </AppBar>
+        <Navbar>
+          <NavBrand>React-Bootstrap</NavBrand>
+        </Navbar>
+        <Tabs>
+          <Tab eventKey={1} title="tab1">tab1</Tab>
+        </Tabs>
       </div>
     );
   }
 }
 
-App.childContextTypes = {muiTheme: React.PropTypes.object};
 
 export default connect(state => {
   return {
+
   }
 })(App);
--- main.jsx
+++ main.jsx
@@ -4,10 +4,10 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 
-// import App from './containers/App';
-// import rootReducer from './reducers/reducer';
+import App from './containers/App';
+import indexReducer from './reducers/index';
 
-let store = createStore(rootReducer);
+let store = createStore(indexReducer);
 
 React.render(
   <Provider store={store}>
