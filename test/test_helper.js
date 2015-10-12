84008e1d0d5a2fad024e591cb38276cd8690316a
muryoimpl <muryoimpl@gmail.com>
2015-10-12 16:10:24 +0900

    Add files for test

---
+++ test/test_helper.js
@@ -0,0 +1,4 @@
+import jsdom from 'mocha-jsdom';
+import React from 'react/addons';
+
+import { expect } from 'chai';
---
+++ test/testdom.js
@@ -0,0 +1,11 @@
+export default function(markup) {
+  if (typeof document !== 'undefined') return;
+
+  var jsdom = require('jsdom').jsdom;
+
+  global.document = jsdom(markup || '');
+  global.window = document.parentWindow;
+  global.navigator = {
+    userAgent: 'node.js'
+  };
+};
